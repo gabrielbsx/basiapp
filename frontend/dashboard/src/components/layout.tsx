e59cd9b56a70cd7879b1fc330e854bb8c7aa0efc
Gabriel <gabrielturing@gmail.com>
2022-11-20 21:09:46 -0300

    feat: layout with bg-slate-900 using tailwindcss

--- frontend/dashboard/src/components/layout.tsx
+++ frontend/dashboard/src/components/layout.tsx
@@ -3,7 +3,7 @@ import Header from './header'
 
 export default function Layout({ children }: IProps) {
   return (
-    <div className="min-h-screen min-w-screen bg-purple-700">
+    <div className="min-h-screen min-w-screen bg-slate-900">
       <Header />
       <div className="">
         {children}
