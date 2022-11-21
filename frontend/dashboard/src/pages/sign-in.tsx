5894cce12dc4d41b6fe40b2a1c51c1fb212ca037
Gabriel <gabrielturing@gmail.com>
2022-11-21 16:42:33 -0300

    refactor: add components on sign-in page

--- frontend/dashboard/src/pages/sign-in.tsx
+++ frontend/dashboard/src/pages/sign-in.tsx
@@ -3,6 +3,9 @@ import Layout from '../components/layout'
 import { AuthContext } from '../contexts/auth-context'
 import { SignInRequestDataType } from '../types'
 import { Formik } from 'formik'
+import Link from 'next/link'
+import InputGuest from '../components/input-guest'
+import ButtonGuest from '../components/button-guest'
 
 export default function SignIn() {
   const { signIn } = useContext(AuthContext)
@@ -48,77 +51,45 @@ export default function SignIn() {
               isSubmitting,
             }) => (
               <form onSubmit={handleSubmit}>
-                <div className="flex flex-col my-5">
-                  <div className="flex flex-row items-center gap-4 relative">
-                    <label
-                      className="flex font-medium sm:text-sm text-gray-100"
-                      htmlFor="email"
-                    >
-                      E-mail
-                    </label>
-                    <input
-                      className="flex w-full px-3 py-2 mt-1 bg-slate-200 border border-slate-700 rounded-md shadow-sm focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
-                      type="email"
-                      name="email"
-                      id="email"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.email}
-                    />
-                  </div>
-                  {errors.email && touched.email && (
-                    <span className="flex text-sm text-red-400 justify-end">
-                      {errors.email}
-                    </span>
-                  )}
-                </div>
-                <div className="flex flex-col mt-5">
-                  <div className="flex flex-row items-center gap-4">
-                    <label
-                      className="flex font-medium sm:text-sm text-gray-100"
-                      htmlFor="email"
-                    >
-                      Senha
-                    </label>
-                    <input
-                      className="flex w-full px-3 py-2 mt-1 bg-slate-200 border border-slate-700 rounded-md shadow-sm focus:outline-none focus:ring-slate-500 focus:border-slate-500 sm:text-sm"
-                      type="password"
-                      name="password"
-                      id="password"
-                      onChange={handleChange}
-                      onBlur={handleBlur}
-                      value={values.password}
-                    />
-                  </div>
-                  {errors.password && touched.password && (
-                    <span className="flex text-sm text-red-400 justify-end">
-                      {errors.password}
-                    </span>
-                  )}
-                </div>
+                <InputGuest
+                  name="email"
+                  label="E-mail"
+                  type="email"
+                  placeholder="Digite seu e-mail"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  error={errors.email}
+                  touched={touched.email}
+                />
+                <InputGuest
+                  name="password"
+                  label="Senha"
+                  type="password"
+                  placeholder="Digite sua senha"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  error={errors.password}
+                  touched={touched.password}
+                />
                 <div className="flex justify-end mb-5">
                   <a className="text-red-400 text-sm font-bold hover:text-red-300 cursor-pointer" href="#">
                     <span>Esqueceu sua senha?</span>
                   </a>
                 </div>
-                <div className="flex justify-center">
-                  <button
-                    className={
-                      `bg-slate-700 text-white rounded-md font-bold px-4 py-2 shadow-md hover:bg-slate-600 focus:outline-none focus:ring-2 focus:ring-slate-600 focus:ring-opacity-50 ${isSubmitting ? 'cursor-not-allowed' : ''}`
-                    }
-                    type="submit"
-                    disabled={isSubmitting}
-                  >
-                    Entrar
-                  </button>
-                </div>
+                <ButtonGuest
+                  type="submit"
+                  isSubmitting={isSubmitting}
+                  text="Entrar"
+                />
               </form>
             )}
           </Formik>
           <div className="flex flex-row justify-end mt-12">
-            <a href="#" className="flex text-white font-bold hover:text-slate-200 gap-1">
+            <Link href="/sign-up" className="flex text-white font-bold hover:text-slate-200 gap-1">
               Não tem uma conta? <span className="text-blue-400">Cadastre-se</span>
-            </a>
+            </Link>
           </div>
         </div>
       </div>
