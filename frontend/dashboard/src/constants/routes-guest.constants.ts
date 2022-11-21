3b371bac27a497c73bbde78e2d86c1c1e3f75c6b
Gabriel <gabrielturing@gmail.com>
2022-11-21 16:40:43 -0300

    feat: add a forget password route on constant route-guest

--- frontend/dashboard/src/constants/routes-guest.constants.ts
+++ frontend/dashboard/src/constants/routes-guest.constants.ts
@@ -1,12 +1,16 @@
 const routes = [
   {
-    path: '/',
-    name: 'Home',
+    path: 'sign-in',
+    name: 'Login',
   },
   {
     path: '/sign-up',
     name: 'Cadastro',
   },
+  {
+    path: '/forgot-password',
+    name: 'Esqueci minha senha',
+  }
 ]
 
 export default routes
