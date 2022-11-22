74105b77915631c3cce9d6c419f4237f7330a3a5
Gabriel <gabrielturing@gmail.com>
2022-11-22 00:27:48 -0300

    feat: add toast container

--- frontend/dashboard/src/pages/_app.tsx
+++ frontend/dashboard/src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../assets/styles/globals.css'
+import 'react-toastify/dist/ReactToastify.css'
 import type { AppProps } from 'next/app'
 import { AuthProvider } from '../contexts/auth-context'
+import { ToastContainer } from 'react-toastify'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
       <Component {...pageProps} />
+      <ToastContainer />
     </AuthProvider>
   )
 }
