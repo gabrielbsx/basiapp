02617b318cafcca908985603598d1c9f0e5a5f35
Gabriel <gabrielturing@gmail.com>
2022-11-22 00:28:09 -0300

    feat: logo component

---
+++ frontend/dashboard/src/components/logo.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+  return (
+    <svg className="md:w-full w-24 text-slate-100" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+      <path fillRule="evenodd" d="M10 2.5L1.5 17.5h17L10 2.5zm0 1.5l6.5 11h-13L10 4z" clipRule="evenodd" />
+    </svg>
+  )
+}
