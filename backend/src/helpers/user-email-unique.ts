d70bed079436061fa09eacf563866b0b1fb32525
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 03:14:28 -0300

    feat: add user email unique validator schema

---
+++ backend/src/helpers/user-email-unique.ts
@@ -0,0 +1,17 @@
+import { ValidationError } from 'joi';
+import User from '../models/user';
+
+export async function UserEmailUnique(email: string) {
+  const user = await User.findOne({ email });
+  if (user) {
+    throw new ValidationError('User already exists', [{
+      message: 'E-mail já cadastrado',
+      path: ['email'],
+      type: 'email.unique',
+      context: {
+        key: 'email',
+        label: 'email',
+      },
+    }], email);
+  }
+}
