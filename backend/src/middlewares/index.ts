4b80de27cd3bd5ac584ebdf61d11ee611f50a781
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:33:45 -0300

    refactor: export user server owner middleware

--- backend/src/middlewares/index.ts
+++ backend/src/middlewares/index.ts
@@ -1,13 +1,16 @@
 import AuthAccountMiddleware from './auth-account/auth-account-middleware';
 import AuthUserMiddleware from './auth-user/auth-user-middleware';
 import UserAdminMiddleware from './user-admin/user-admin-middleware';
+import UserServerOwnerMiddleware from './user-server-owner/user-server-owner-middleware';
 
 const authUserMiddleware = new AuthUserMiddleware();
 const userAdminMiddleware = new UserAdminMiddleware();
 const authAccountMiddleware = new AuthAccountMiddleware();
+const userServerOwnerMiddleware = new UserServerOwnerMiddleware();
 
 export {
   authUserMiddleware,
   userAdminMiddleware,
   authAccountMiddleware,
+  userServerOwnerMiddleware,
 };
