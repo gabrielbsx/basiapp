cfde3babc6ad892a2d23ea2d5bd827e03b1c0e05
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:30:26 -0300

    refactor: move validators to validator folder

---
+++ backend/src/validators/user-email-exists.ts
@@ -0,0 +1,18 @@
+import { ValidationError } from 'joi';
+import User from '../models/user';
+
+export async function UserEmailExists(email: string) {
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw new ValidationError('User not found', [{
+      message: 'Usuário não encontrado',
+      path: ['email'],
+      type: 'email.exists',
+      context: {
+        key: 'email',
+        label: 'email',
+        value: email,
+      },
+    }], email);
+  }
+}
