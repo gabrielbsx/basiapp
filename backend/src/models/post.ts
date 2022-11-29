879a69d3e546f8feaac2fd8873e2e9b7d019fdd1
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:22:48 -0300

    refactor: add types to post model

--- backend/src/models/post.ts
+++ backend/src/models/post.ts
@@ -1,12 +1,27 @@
 import database from '../config/database';
 
-const postSchema = new database.Schema({
+export interface IPost {
+  title: string;
+  content: string;
+  thumbnail: string;
+  category: database.Schema.Types.ObjectId;
+  server: database.Schema.Types.ObjectId;
+  account: database.Schema.Types.ObjectId;
+  likes: database.Schema.Types.ObjectId[];
+  comments: database.Schema.Types.ObjectId[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const postSchema = new database.Schema<IPost>({
   title: { type: String, required: true },
   content: { type: String, required: true },
   thumbnail: { type: String, required: false },
   category: { type: database.Schema.Types.ObjectId, ref: 'PostCategory', required: false },
   likes: [{ type: database.Schema.Types.ObjectId, ref: 'PostLike' }],
   comments: [{ type: database.Schema.Types.ObjectId, ref: 'Post' }],
+  account: { type: database.Schema.Types.ObjectId, ref: 'Account', required: true },
+  server: { type: database.Schema.Types.ObjectId, ref: 'Server', required: true },
 }, { timestamps: true });
 
 export default database.model('Post', postSchema);
