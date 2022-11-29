80d1245d65768235398035e138d7768b645092d4
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:34:11 -0300

    refactor: export create server controller

--- backend/src/usecases/index.ts
+++ backend/src/usecases/index.ts
@@ -1,13 +1,16 @@
 import AuthUserController from './auth-user/auth-user-controller';
+import CreateServerController from './create-server/create-server-controller';
 import CreateUserController from './create-user/create-user-controller';
 import UpdateUserController from './update-user/update-user-controller';
 
 const createUserController = new CreateUserController();
 const updateUserController = new UpdateUserController();
 const authUserController = new AuthUserController();
+const createServerController = new CreateServerController();
 
 export {
   createUserController,
   updateUserController,
   authUserController,
+  createServerController,
 };
