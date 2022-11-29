b31e1d26090677412046c96f69e538d8e666835a
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:34:42 -0300

    feat: add create server and update server routes

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -3,9 +3,12 @@ import {
   createUserController,
   updateUserController,
   authUserController,
+  createServerController,
 } from './usecases';
 import {
   authUserMiddleware,
+  userAdminMiddleware,
+  userServerOwnerMiddleware,
 } from './middlewares';
 
 const routes = Router();
@@ -14,5 +17,8 @@ routes.post('/users', createUserController.handle);
 routes.post('/users/auth', authUserController.handle);
 
 routes.put('/users', authUserMiddleware.handle, updateUserController.handle);
+
+routes.post('/servers', authUserMiddleware.handle, userAdminMiddleware.handle, createServerController.handle);
+routes.post('/servers/:id', authUserMiddleware.handle, userServerOwnerMiddleware.handle, createServerController.handle);
 
 export default routes;
