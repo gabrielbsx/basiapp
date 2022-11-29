53025e205284ee58d7aca72e798fcc0da5ddcc9e
Gabriel Barbosa <gsilva.1996@alunos.utfpr.edu.br>
2022-11-28 22:22:38 -0300

    refactor: add types to user model

--- backend/src/models/user.ts
+++ backend/src/models/user.ts
@@ -1,12 +1,23 @@
-// mongoose user model
+import { Schema } from 'mongoose';
 import database from '../config/database';
+import { IServer } from './server';
 
-const userSchema = new database.Schema({
-  email: { type: String, unique: true, lowercase: true },
+export type IUser = {
+  name?: string;
+  email: string;
+  password: string;
+  role: string;
+  servers?: IServer[];
+  createdAt: Date;
+  updatedAt: Date;
+}
+
+const userSchema = new database.Schema<IUser>({
   name: { type: String, required: false },
+  email: { type: String, unique: true, lowercase: true },
+  password: { type: String, required: true },
   role: { type: String, required: true, default: 'user', enum: ['user', 'admin'] },
   servers: [{ type: database.Schema.Types.ObjectId, ref: 'Server' }],
-  password: { type: String, required: true },
 }, { timestamps: true });
 
 export default database.model('User', userSchema);
