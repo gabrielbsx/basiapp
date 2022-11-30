65e524ba16fb0d74de094b1dba6e0b2140eda33d
Gabriel <gabrielturing@gmail.com>
2022-11-30 02:16:56 -0300

    feat: ensure that server can be updated

--- backend/src/usecases/update-server/update-server-validator.ts
+++ backend/src/usecases/update-server/update-server-validator.ts
@@ -1,26 +1,19 @@
 import * as Joi from 'joi';
-import { UserEmailUnique } from '../../validators';
-import { userMessages } from '../../validators/messages';
+import { ServerNameUnique } from '../../validators';
+import { serverMessages } from '../../validators/messages';
 import { Validator } from '../../validators';
+import { ServerIdExists } from '../../validators/server-id-exists';
 
-export class UpdateUserValidator extends Validator {
-  static messages = userMessages;
+export class UpdateServerValidator extends Validator {
+  static messages = serverMessages;
   static validationSchema: Joi.ObjectSchema = Joi.object({
-    name: Joi.string().optional()
-      .messages(UpdateUserValidator.messages.name),
-    email: Joi.string().email().required()
-      .external(UserEmailUnique)
-      .messages(UpdateUserValidator.messages.email),
-    currentPassword: Joi.string().optional()
-      .messages(UpdateUserValidator.messages.currentPassword),
-    password: Joi.string().optional()
-      .min(6).max(24)
-      .messages(UpdateUserValidator.messages.password),
-    passwordConfirmation: Joi.string()
-      .when('password', {
-        is: Joi.exist(),
-        then: Joi.required().valid(Joi.ref('password')),
-      })
-      .messages(UpdateUserValidator.messages.passwordConfirmation),
+    serverId: Joi.string().required()
+      .external(ServerIdExists)
+      .messages(UpdateServerValidator.messages.serverId),
+    name: Joi.string().optional().min(3)
+      .external(ServerNameUnique)
+      .messages(UpdateServerValidator.messages.name),
+    description: Joi.string().optional().min(3)
+      .messages(UpdateServerValidator.messages.description),
   });
 } 
