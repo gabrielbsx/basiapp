19797dded74a0fb37735cf3afb98cd70128ce5ca
Gabriel <gabrielturing@gmail.com>
2022-11-30 02:19:36 -0300

    feat: ensure that account is authenticate

--- backend/src/usecases/auth-account/auth-account-validator.ts
+++ backend/src/usecases/auth-account/auth-account-validator.ts
@@ -1,13 +1,17 @@
 import * as Joi from 'joi';
 import { Validator } from '../../validators';
 import { accountMessages } from '../../validators/messages';
+import { ServerIdExists } from '../../validators/server-id-exists';
 
 export class AuthAccountValidator extends Validator {
   static messages = accountMessages;
   static validationSchema: Joi.ObjectSchema = Joi.object({
-    username: Joi.string().min(4).max(10)
+    serverId: Joi.string().required()
+      .external(ServerIdExists)
+      .messages(AuthAccountValidator.messages.serverId),
+    username: Joi.string().min(4).max(12).required()
       .messages(AuthAccountValidator.messages.username),
-    password: Joi.string().min(4).max(10).required()
+    password: Joi.string().min(4).max(12).required()
       .messages(AuthAccountValidator.messages.password),
   });
 }
